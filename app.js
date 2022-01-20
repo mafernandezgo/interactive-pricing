cd185918d7bdb024f78e91b989eb871bec2a8f60
mafernandezgo <78058092+mafernandezgo@users.noreply.github.com>
2022-01-20 15:34:46 -0500

    fixing js

--- app.js
+++ app.js
@@ -6,78 +6,28 @@ window.addEventListener  ("load", async () =>   {
     const container = document.querySelector(".sliderRange")
     const bolita = document.querySelector(".discount")
 
-
     container.addEventListener("input",myFunction )
 
-        function myFunction() {
+    function myFunction() {
         let rangeValue = parseInt(container.value)
-        numPageViews.innerHTML = rangeValue;
+        let views = [10, 50, 100, 500, 1000]
+        let monthValue = [8,12,16,24,36]
+        let anualValue = monthValue.map( x => x*12*0.75 )
+        price.innerHTML = monthValue[rangeValue]
+        numPageViews.innerHTML = views[rangeValue]
+                
+        var colorProgress = 'linear-gradient(90deg, hsl(174, 86%, 45%)'  + rangeValue*25 + '%, hsl(224, 65%, 95%)' + rangeValue*25 + '%)' 
+        container.style.background = colorProgress
         
-        let monthValue 
-        // switch (rangeValue){
-            //     case 10:
-            //         monthValue = 8;
-            //         price.innerHTML = monthValue
-            //         numPageViews.innerHTML = rangeValue
-            //         break;
-            //     case 50:
-            //         monthValue = 12;
-            //         price.innerHTML = monthValue
-            //         numPageViews.innerHTML = rangeValue
-            //         break;
-            //     case 100:
-            //         monthValue = 16;
-            //         price.innerHTML = monthValue
-            //         numPageViews.innerHTML = rangeValue
-            //         break;
-            //     case 500:
-            //         monthValue = 26;
-            //         price.innerHTML = monthValue
-            //         numPageViews.innerHTML = rangeValue
-            //         break;
-            //     case 1000:
-            //         monthValue = 36;
-            //         price.innerHTML = monthValue
-            //         numPageViews.innerHTML = rangeValue
-            //         break;
-            // }   
-            
-            if (rangeValue <= 10){
-                monthValue = 8;
-                price.innerHTML = monthValue
-                numPageViews.innerHTML = rangeValue
-            } else if (rangeValue <= 50 ) {
-                monthValue = 12;
-                price.innerHTML = monthValue
-                numPageViews.innerHTML = rangeValue
-            } else if ( rangeValue <= 100 ){ 
-                monthValue = 16;
-                price.innerHTML = monthValue
-                numPageViews.innerHTML = rangeValue
-            } else if (rangeValue <= 500 ) {
-                monthValue = 26;
-                price.innerHTML = monthValue
-                numPageViews.innerHTML = rangeValue
-            } else if ( rangeValue <= 1000){
-                monthValue = 36;
-                price.innerHTML = monthValue
-                numPageViews.innerHTML = rangeValue}
-                
-                var colorProgress = 'linear-gradient(90deg, hsl(174, 86%, 45%)'  + rangeValue/10 + '%, hsl(224, 65%, 95%)' + rangeValue/10 + '%)' 
-                container.style.background = colorProgress
-                
-                let anualValue = monthValue*12*0.75
-                
-                checkbox.addEventListener('change',() => {
-                    if( !checkbox.checked){
-                        price.innerHTML = monthValue
-                        payment.innerHTML = "/month"
-                    } else if (checkbox.checked){
-                        price.innerHTML = anualValue
-                        payment.innerHTML = "/year"
-                    } 
-                })
-
+        checkbox.addEventListener('change',() => {
+            if( !checkbox.checked){
+                price.innerHTML = monthValue[rangeValue]
+                payment.innerHTML = "/month"
+            } else if (checkbox.checked) {
+                price.innerHTML = anualValue[rangeValue]
+                payment.innerHTML = "/year"
+            } 
+        })
     }
 
     bolita.addEventListener("mouseup", () =>{
