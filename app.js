14cffea24de122b982040a3049658572af4a2ac8
mafernandezgo <78058092+mafernandezgo@users.noreply.github.com>
2022-01-21 11:19:48 -0500

    fixing styles

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-window.addEventListener  ("load", async () =>   {
-    const numPageViews = document.querySelector(".numPageViews")
-    const price = document.querySelector(".priceValue")
-    const checkbox = document.querySelector(".checkbox")
-    const payment = document.querySelector(".payment")
-    const container = document.querySelector(".sliderRange")
-    const bolita = document.querySelector(".discount")
+const numPageViews = document.querySelector(".numPageViews")
+const price = document.querySelector(".priceValue")
+const checkbox = document.querySelector(".checkbox")
+const payment = document.querySelector(".payment")
+const container = document.querySelector(".sliderRange")
+const views = [10, 50, 100, 500, 1000]
+const prices = {
+    monthly: [8,12,16,24,36],
+    annual: [72,108,144,216,324]
+} 
 
-    container.addEventListener("input",myFunction )
+let selectedOption = "monthly"
+let rangeValue
 
-    function myFunction() {
-        let rangeValue = parseInt(container.value)
-        let views = [10, 50, 100, 500, 1000]
-        let monthValue = [8,12,16,24,36]
-        let anualValue = monthValue.map( x => x*12*0.75 )
-        price.innerHTML = monthValue[rangeValue]
-        numPageViews.innerHTML = views[rangeValue]
-                
-        var colorProgress = 'linear-gradient(90deg, hsl(174, 86%, 45%)'  + rangeValue*25 + '%, hsl(224, 65%, 95%)' + rangeValue*25 + '%)' 
-        container.style.background = colorProgress
-        
-        checkbox.addEventListener('change',() => {
-            if( !checkbox.checked){
-                price.innerHTML = monthValue[rangeValue]
-                payment.innerHTML = "/month"
-            } else if (checkbox.checked) {
-                price.innerHTML = anualValue[rangeValue]
-                payment.innerHTML = "/year"
-            } 
-        })
-    }
+function priceUpdate() {
+    rangeValue = parseInt(container.value)
+    price.innerHTML = prices[selectedOption][rangeValue]
+    payment.innerHTML = checkbox.checked ? "/year": payment.innerHTML = "/month"
+}
 
-    bolita.addEventListener("mouseup", () =>{
-        bolita.style.background = 'hsl(174, 86%, 45%)'
-        console.log("me esta escuchando")
-        container.addEventListener("mousedown", () => {
-        bolita.style.background = "red"
-        })
-    })
+container.addEventListener("input", () => {
+    priceUpdate()
+    let colorProgress = 'linear-gradient(90deg, hsl(174, 86%, 45%)'  + rangeValue*25 + '%, hsl(224, 65%, 95%)' + rangeValue*25 + '%)' 
+    numPageViews.innerHTML = views[rangeValue]
+    container.style.background = colorProgress
+})
+
+checkbox.addEventListener("change",() => {
+    if (!checkbox.checked) {
+        selectedOption = "monthly"
+        priceUpdate()
+    } else if(checkbox.checked){
+        selectedOption = "annual"
+        priceUpdate()
+    } 
 })
 
